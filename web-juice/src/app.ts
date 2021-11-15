bde727facf7041df590fd790db311959c87899a6
Emilia <me@lazerwalker.com>
2021-11-15 16:07:25 +0000

    Add some test juice

--- web-juice/src/app.ts
+++ web-juice/src/app.ts
@@ -16,7 +16,47 @@ function spawnPopup() {
   console.log(`${random(0, 70)}vw;`, $el.style.left, $el.style.top)
 
   $el.addEventListener("click", (e) => {
-    $el.remove()
+    const hide = $el.animate(
+      [
+        { transform: "scale(1)"},
+        { transform: "scale(0)"}
+      ],
+      {
+       duration: 40
+      }
+    )
+    hide.onfinish = () => $el.remove()
   })
   document.body.appendChild($el)
+  $el.animate(
+    [
+      { transform: "scale(0)"},
+      { transform: "scale(1)"}
+    ],
+    {
+      duration: 20
+    }
+  )
+  screenshake()
+} 
+
+function screenshake() {
+  document.body.animate(
+    [
+      { transform: "translate(1px, 1px) rotate(0deg)" },
+      { transform: "translate(-1px, -1px) rotate(-1deg)" },
+      { transform: "translate(-1px, 0px) rotate(1deg)" },
+      { transform: "translate(0px, 1px) rotate(0deg)" },
+      { transform: "translate(1px, -1px) rotate(1deg)" },
+      { transform: "translate(-1px, 1px) rotate(-1deg)" },
+      { transform: "translate(-2px, 1px) rotate(0deg)" },
+      { transform: "translate(2px, 1px) rotate(-1deg)" },
+      { transform: "translate(-1px, -1px) rotate(1deg)" },
+      { transform: "translate(0px, 1px) rotate(0deg)" },
+      { transform: "translate(0px, -1px) rotate(-1deg)" },
+    ],
+    {
+      duration: 20,
+    }
+  );
 }
